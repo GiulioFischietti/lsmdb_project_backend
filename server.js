a34e188129ad7167c56d6db20cff08a52662a712
GiulioFischietti <fischiettigiulio@gmail.com>
2022-03-24 01:47:57 +0100

    participates/follows entitybyid

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const neo4j = require('neo4j-driver')
 var { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
 
@@ -10,6 +11,19 @@ const { db } = require('../project_backend/myExpressApp/models/event');
 const client = new MongoClient(process.env.DATABASE_URL);
 
 // const appRouter = require('./routes/approuter')
+
+const {
+    GRAPHDB_URL,
+    db_username,
+    db_password,
+    database,
+} = process.env
+var driver = neo4j.driver(
+    'bolt://localhost',
+    neo4j.auth.basic('neo4j', 'ssss')
+)
+
+const session = driver.session();
 
 
 app.use(bodyParser.json());
@@ -44,6 +58,9 @@ app.post('/signup', async (req, res) => {
 
             const hashedPassword = await bcrypt.hash(req.body.password, 10)
             const newUser = await client.db('needfy').collection('users').insertOne({ "name": req.body.name, "email": req.body.email, "psw": hashedPassword, "created_at": new Date() })
+            const result = await session.run('CREATE (n:User {name: "' + req.body.name + '" , _id: "' + newUser.insertedId + '" , email: "' + req.body.email + '" })')
+
+            // CREATE (n:Person {name: req.body.name, email: req.body.email, psw: hashedPassword, created_at: new Date()  })
             res.status(200).send({ success: true, data: newUser });
         }
         else {
@@ -115,11 +132,12 @@ app.post('/searchevents', async (req, res) => {
                 }
             }
         }
-        req.body.start != null ? parameters.start = { $gte: (new Date(req.body.start)).toISOString() } : null
-        req.body.name != null ? parameters.name = { $regex: req.body.name } : null
-        req.body.genres != null ? parameters.genres = { $in: req.body.genres } : null
-        
+        if (req.body.start != null) parameters.start = { $gte: (new Date(req.body.start)).toISOString() }
+        if (req.body.name != null) parameters.name = { $regex: req.body.name }
+        if (req.body.genres != null && req.body.genres != []) parameters.genres = { $in: req.body.genres }
+
         const searched_events = await client.db('needfy').collection('events').find(parameters).sort({ "start": -1 }).limit(8).toArray()
+
         res.status(200).send({ "success": true, "data": searched_events })
 
     } catch (error) {
@@ -150,12 +168,16 @@ app.post('/nearclubs', async (req, res) => {
         res.status(500).send({ "error": error })
     }
 })
+
 app.get('/organizerbyid', async (req, res) => {
     try {
 
         var organizer = await client.db('needfy').collection('organizers').findOne({
             _id: ObjectId(req.query._id)
         })
+        const result = await session.run("match (a:User), (b:Organizer) where a._id = '" + req.query.user_id + "' and b._id = '" + req.query._id + "' return exists((a)-[:Follows]->(b));")
+        organizer.is_followed = result.records[0]._fields[0];
+        
         res.status(200).send({ "success": true, "data": organizer })
 
     } catch (error) {
@@ -167,11 +189,14 @@ app.get('/organizerbyid', async (req, res) => {
 app.get('/eventbyid', async (req, res) => {
     try {
 
-        console.log(req.query)
+        // const result = await session.run('match (a:User)-[r:Follows]->(b:Club) where a.id = 1  and b.name = 'Lumiere' return r;')
 
         var event = await client.db('needfy').collection('events').findOne({
             _id: ObjectId(req.query._id)
         })
+        const result = await session.run("match (a:User), (b:Event) where a._id = '" + req.query.user_id + "' and b._id = '" + req.query._id + "' return exists((a)-[:ParticipatesIn]->(b));")
+        event.participates = result.records[0]._fields[0];
+        
         res.status(200).send({ "success": true, "data": event })
 
     } catch (error) {
@@ -185,7 +210,9 @@ app.get('/clubbyid', async (req, res) => {
 
         var club = await client.db('needfy').collection('clubs').findOne({
             _id: ObjectId(req.query._id)
-        })
+        }) 
+        const result = await session.run("match (a:User), (b:Club) where a._id = '" + req.query.user_id + "' and b._id = '" + req.query._id + "' return exists((a)-[:Follows]->(b));")
+        club.is_followed = result.records[0]._fields[0];
         res.status(200).send({ "success": true, "data": club })
 
     } catch (error) {
@@ -208,9 +235,9 @@ app.get('/userbyid', async (req, res) => {
 })
 
 app.post('/edituser', async (req, res) => {
-    const params = {...req.body};
+    const params = { ...req.body };
     delete params._id;
-    
+
     try {
         await client.db('needfy').collection('users').updateOne({
             _id: ObjectId(req.body._id)
